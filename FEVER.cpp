f045cea9e6fd8040e12d343a57c6b93df0602317
kingpin-ay <ayushmondal001@gmail.com>
2022-09-13 23:43:21 +0530

    The basic fever question is added for the code chef practice set

---
+++ FEVER.cpp
@@ -0,0 +1,23 @@
+#include "bits/stdc++.h"
+using namespace std;
+
+
+int main()
+{
+	int test_case ;
+	cin >> test_case ;
+
+	while(test_case--)
+	{
+		int temperature;
+		cin >> temperature;
+
+		if(temperature > 98)
+		{
+			cout << "YES" << "\n";
+		}
+		else{
+			cout << "NO" << "\n";
+		}
+	}
+}
